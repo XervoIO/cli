614382cb4a18a63b1e303083a9e09487488f7813
Jack Boberg <hello@jackboberg.info>
2017-02-03 22:06:16 -0500

    style: Fix most standard issues
    These changes are primarily:
    
        > standard --fix
    
    I also cleaned up some of the safe decisions like `one-var` and
    `operator-linebreak`. There are still standard errors in this commit:
    
        > npm run lint | wc -l
        standard: Use JavaScript Standard Style (http://standardjs.com)
              24

--- test/controllers/project.js
+++ test/controllers/project.js
@@ -1,73 +1,73 @@
-const Controller = require('../../lib/controllers/project.js');
+const Controller = require('../../lib/controllers/project.js')
 
-const Code = require('code');
-const Lab = require('lab');
+const Code = require('code')
+const Lab = require('lab')
 
-var lab = exports.lab = Lab.script();
-var describe = lab.describe;
-var it = lab.it;
-var expect = Code.expect;
+var lab = exports.lab = Lab.script()
+var describe = lab.describe
+var it = lab.it
+var expect = Code.expect
 
 describe('projectController', function () {
   describe('project.isSupported', function () {
-    var runtime, srcVersion, supported;
+    var runtime, srcVersion, supported
 
     describe('project.isSupported passes', function () {
       it('returns true when version is greater than min version', function (done) {
-        supported = Controller.isSupported('1.3.3', 'Meteor');
-        expect(supported).to.equal(true);
-        done();
-      });
+        supported = Controller.isSupported('1.3.3', 'Meteor')
+        expect(supported).to.equal(true)
+        done()
+      })
 
       it('return true when runtime is not Meteor', function (done) {
-        supported = Controller.isSupported('5.9.0', 'Node.js');
-        expect(supported).to.equal(true);
-        done();
-      });
+        supported = Controller.isSupported('5.9.0', 'Node.js')
+        expect(supported).to.equal(true)
+        done()
+      })
 
       it('returns true on passing non-semver version', function (done) {
-        var nonSemver = ['1.3.3.4', '1.4', '1.5-beta', '2.alpha', '2-alpha', '3'];
+        var nonSemver = ['1.3.3.4', '1.4', '1.5-beta', '2.alpha', '2-alpha', '3']
         nonSemver.forEach(function (version) {
-          supported = Controller.isSupported(version, 'Meteor');
-          expect(supported).to.equal(true);
-        });
-        done();
-      });
+          supported = Controller.isSupported(version, 'Meteor')
+          expect(supported).to.equal(true)
+        })
+        done()
+      })
 
       it('returns true when major is greater than min supported major', function (done) {
-        supported = Controller.isSupported('2.0.0', 'Meteor');
-        expect(supported).to.equal(true);
-        done();
-      });
-    });
+        supported = Controller.isSupported('2.0.0', 'Meteor')
+        expect(supported).to.equal(true)
+        done()
+      })
+    })
 
     describe('project.isSupported fails', function () {
       it('returns false when version is less than min supported version', function (done) {
-        supported = Controller.isSupported('1.2.5', 'Meteor');
-        expect(supported).to.equal(false);
-        done();
-      });
+        supported = Controller.isSupported('1.2.5', 'Meteor')
+        expect(supported).to.equal(false)
+        done()
+      })
 
       it('returns false when major does not pass', function (done) {
-        supported = Controller.isSupported('0.3.4', 'Meteor');
-        expect(supported).to.equal(false);
-        done();
-      });
+        supported = Controller.isSupported('0.3.4', 'Meteor')
+        expect(supported).to.equal(false)
+        done()
+      })
 
       it('returns false when minor does not pass', function (done) {
-        supported = Controller.isSupported('1.2.4', 'Meteor');
-        expect(supported).to.equal(false);
-        done();
-      });
+        supported = Controller.isSupported('1.2.4', 'Meteor')
+        expect(supported).to.equal(false)
+        done()
+      })
 
       it('returns false on failing non-semver version', function (done) {
-        var nonSemver = ['0', '0.1', '1', '1.2-alpha', 'infinite'];
+        var nonSemver = ['0', '0.1', '1', '1.2-alpha', 'infinite']
         nonSemver.forEach(function (version) {
-          supported = Controller.isSupported(version, 'Meteor');
-          expect(supported).to.equal(false);
-        });
-        done();
-      });
-    });
-  });
-});
+          supported = Controller.isSupported(version, 'Meteor')
+          expect(supported).to.equal(false)
+        })
+        done()
+      })
+    })
+  })
+})
